e3c500fc03181c88d076ab68ab16b76baa4fafd1
chandan <singhchn08@gmail.com>
2025-07-17 23:20:53 +0530

    test1

---
+++ dl/gcc-10.5.0-nolibc/arm-linux-gnueabi/lib/gcc/arm-linux-gnueabi/10.5.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/arnd/git/gcc/configure --host=x86_64-linux-gnu --build=aarch64-linux --target=arm-linux-gnueabi --enable-targets=all --prefix=/home/arnd/cross/x86_64/gcc-10.5.0-nolibc/arm-linux-gnueabi --enable-languages=c --without-headers --disable-bootstrap --disable-nls --disable-threads --disable-shared --disable-libmudflap --disable-libssp --disable-libgomp --disable-decimal-float --disable-libquadmath --disable-libatomic --disable-libcc1 --disable-libmpx --enable-checking=release --with-static-standard-libraries";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "tls", "gnu" } };
